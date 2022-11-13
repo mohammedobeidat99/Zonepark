d4a245c7b9f1bc3196de275ac4fdd4fdaeddea20
Mohammed <obeidatmohammed80@gmail.com>
2022-11-14 01:21:38 +0300

    Scan

--- app/src/main/java/com/example/myapplication/Scan_page.java
+++ app/src/main/java/com/example/myapplication/Scan_page.java
@@ -1,16 +1,21 @@
 package com.example.myapplication;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class Scan_page extends AppCompatActivity   {
-        private Button update;
+        private Button scan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,42 +23,44 @@ public class Scan_page extends AppCompatActivity   {
         setContentView(R.layout.activity_scan_page);
 
 
+        scan = (Button) findViewById(R.id.scan);
 
-        update=findViewById(R.id.update);
-
-        //String ss="null";
-        Intent intent1 = getIntent();
-        String num=intent1.getStringExtra("num");
-        Intent intent2 = getIntent();
-        String ss = intent2.getStringExtra("s1");
-
-
-        update.setOnClickListener(new View.OnClickListener() {
-
+        scan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                if (ss.equals("true")) {
-
-                    FirebaseDatabase.getInstance().getReference("mallinfo").child("Amman").child("Maka Mall").child("space").child(num).child("status").setValue(false);
-                }
-                else if (ss.equals("false")) {
-
-                    FirebaseDatabase.getInstance().getReference("mallinfo").child("Amman").child("Maka Mall").child("space").child(num).child("status").setValue(true);
-                }
-
-
-
-              /*  if (s.equals("false")) {
-                    Toast.makeText(view.getContext(), "Not Available" , Toast.LENGTH_SHORT).show();
-
-                    FirebaseDatabase.getInstance().getReference("mallinfo").child("Amman").child("Maka Mall").child("space").child("1").child("status").setValue(true);
-                }*/
-
-
+                IntentIntegrator intentIntegrator = new IntentIntegrator(Scan_page.this);
+                intentIntegrator.setPrompt("Use volume up key for Flash on/ volume down key for flash off");
+                intentIntegrator.setBeepEnabled(true);
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.setCaptureActivity(Capture.class);
+                intentIntegrator.initiateScan();
             }
         });
 
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode,
+                resultCode, data);
+        // test if the content is not null :
+
+        if (intentResult.getContents() != null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(Scan_page.this);
+            builder.setTitle("Results");
+            builder.setMessage(intentResult.getContents());
+            builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            builder.show();
+        } else {
+            Toast.makeText(this, "Sory you did not scan anything ! ", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
